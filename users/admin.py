0c4d1a140bb415c9a30cfd32ead505526131d063
Haider Khan <haiderkhan.live@gmail.com>
2019-10-30 14:40:21 +0500

    remove users/form.py

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin, Group
 from .models import CustomUser
-from .forms import CustomUserForm
+
 
 # Register your models here.
 
@@ -11,8 +11,6 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ['username', 'email', 'first_name', 'last_name']
     list_filter = ['is_superuser', 'is_active', 'is_staff']
     list_display = ['username', 'email', 'first_name', 'last_name','is_superuser', 'is_active', 'is_staff']
-
-    form = CustomUserForm
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
