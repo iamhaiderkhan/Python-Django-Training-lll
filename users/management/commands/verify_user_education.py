82ec736556c13254a515624771ccfc4b16978237
Haider Khan <haiderkhan.live@gmail.com>
2019-11-04 15:52:48 +0500

    urlencoded + change duplicate code

--- users/management/commands/verify_user_education.py
+++ users/management/commands/verify_user_education.py
@@ -4,12 +4,14 @@ from django.contrib.auth import get_user_model
 from users.models import EducationInformation
 from django.core.exceptions import ObjectDoesNotExist
 from users.serializers import UserEducationInformationSerializer
+from urllib.parse import urlencode
 User = get_user_model()
+
 
 class Command(BaseCommand):
 
     help = "Verify User Education"
-
+    degree_ids = list()
 
     def add_arguments(self, parser):
         parser.add_argument('id', type=int, nargs='?', help='Define a user id')
@@ -18,32 +20,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         is_all = options['all']
         user_id = options['id']
-        degree_ids = list()
+
         if is_all:
             users = User.objects.prefetch_related('educations')
-            for user in users:
-                user_education_info = user.educations.order_by('-degree_completed_date').first()
-                if user_education_info:
-                    context = UserEducationInformationSerializer(user_education_info).data
-                    response = self.verify_degree_api(user.username, **context).json()
-
-                    if len(response):
-                        degree_ids.append(user_education_info.id)
-
+            self.stdout.write("Total Number of Users: {0}".format(len(users)))
+            for index, user in enumerate(users, start=1):
+                self.verify_user_education_info(user, index)
         elif user_id:
             try:
                 user = User.objects.prefetch_related('educations').get(id=user_id)
-                user_education_info = user.educations.order_by('-degree_completed_date').first()
-                if user_education_info:
-                    context = UserEducationInformationSerializer(user_education_info).data
-                    response = self.verify_degree_api(user.username, **context).json()
-                    if len(response):
-                        degree_ids.append(user_education_info.id)
+                self.verify_user_education_info(user)
             except ObjectDoesNotExist as e:
                 self.stdout.write("Invalid id, please enter valid user id.")
+        if len(self.degree_ids):
+            EducationInformation.objects.filter(pk__in=self.degree_ids).update(is_verified =True)
 
-        if len(degree_ids):
-            EducationInformation.objects.filter(pk__in=degree_ids).update(is_verified =True)
+    def verify_user_education_info(self, user, index=1):
+        user_education_info = user.educations.order_by('-degree_completed_date').first()
+        if user_education_info:
+            context = UserEducationInformationSerializer(user_education_info).data
+            response = self.verify_degree_api(user.username, **context).json()
+            self.stdout.write("{0}: Username:{1}, Education Status:{2}".format(index, user.username, "verified" if len(response) else "un-verified"))
+            if len(response):
+                self.degree_ids.append(user_education_info.id)
+        else:
+            self.stdout.write(
+                "{0}: Username:{1}, Education Status:{2}".format(index, user.username,"un-verified"))
 
     def verify_degree_api(self, username, **options):
         params = dict()
@@ -51,4 +53,4 @@ class Command(BaseCommand):
         params['username'] = username
         params['degree_name'] = options['degree_name']
         params['cgpa'] = options['cgpa']
-        return requests.get(url=url,params=params)
+        return requests.get(url=url, params=urlencode(params))
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .serializers import UserSerializer, UserEducationInformationSerializer, DegreeSerializer
 from .models import EducationInformation, Degree
 # Create your views here.
@@ -27,5 +29,8 @@ class DegreeVerifyViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = self.queryset
         query_params = self.request.query_params
         if query_params:
-            queryset = Degree.objects.filter(username=query_params.get('username'),degree_name=query_params.get('degree_name'),cgpa=query_params.get('cgpa'))
+            try:
+                queryset = Degree.objects.filter(username=query_params.get('username'),degree_name=query_params.get('degree_name'),cgpa=query_params.get('cgpa'))
+            except NotFound as e:
+                return Response(status.HTTP_404_NOT_FOUND)
         return queryset
